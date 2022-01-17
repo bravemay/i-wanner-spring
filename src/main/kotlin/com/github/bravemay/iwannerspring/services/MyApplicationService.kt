7fbaebd59d681c7ab28162a52204d275ac5aaa8c
GitHub Action <action@github.com>
2022-01-17 13:28:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/bravemay/iwannerspring/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bravemay/iwannerspring/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bravemay.iwannerspring.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bravemay.iwannerspring.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bravemay/iwannerspring/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bravemay/iwannerspring/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bravemay.iwannerspring.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bravemay.iwannerspring.MyBundle
 
 class MyProjectService(project: Project) {
 
